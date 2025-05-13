1d7ffff98071e1a1fa48f032c90d50df9331f128
Reem Hassan <reemhassan742r@gmail.com>
2025-05-13 23:14:20 +0300

    Doc 1

--- CSharpFundamentals/CSharpFundamentals.ConsoleApp/Program.cs
+++ CSharpFundamentals/CSharpFundamentals.ConsoleApp/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+String[] names = ["Reem", "Mo", "Yoyo"];
+String name = names[^1];
+Console.WriteLine(name);
+Console.WriteLine(args.Length);
